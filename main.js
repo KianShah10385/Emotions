6518342fa36768fde060e824115dbc52f0246af8
KianShah10385 <108907854+KianShah10385@users.noreply.github.com>
2023-02-21 20:58:40 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -8,13 +8,11 @@ Webcam.set({
 camera = document.getElementById("camera");
 
 Webcam.attach ('#camera');
-
 function take_snapshot()
-{
-    Webcam.snap(function(data_uri) {
-        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
-    });
-}
+ { Webcam.snap(function(data_uri)
+     { document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+     }); }
+
 console.log('ml5 version', ml5.version);
 classifier = ml5.imageClassifier(' https://teachablemachine.withgoogle.com/models/mJefg2NCG/model.json',modelLoaded);
 function modelLoaded()
